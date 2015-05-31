176f5a17566f82f95dc709914398cc59e34c4288
Eduardo Bouças <mail@eduardoboucas.com>
2015-05-31 23:18:04 +0100

    Polish up ServiceWorker

--- js/app.js
+++ js/app.js
@@ -9,22 +9,8 @@ define([
 
 	var initialize = function () {
 		bindUiVideoEvents();
-		startServiceWorker();
-
+		
 		Router.initialize(Modernizr.videoautoplay, videoMinWidth);
-	}
-
-	var startServiceWorker = function () {
-		if ('serviceWorker' in navigator) {
-			navigator.serviceWorker.register('/serviceWorker.js')
-				.then(function(registration) {
-					//showNotification('Website cached for offline use.');
-					console.log('ServiceWorker registration successful');
-				})
-				.catch(function(err) {
-					console.log('ServiceWorker registration failed: ', err);
-				});
-		}		
 	}
 
 	var bindUiVideoEvents = function () {
@@ -33,28 +19,6 @@ define([
 
 			return false;
 		});
-	}
-
-	var showNotification = function (message) {
-		$(createNotification(message)).appendTo('body').hide().fadeIn('slow');
-
-		setTimeout(function () {
-			$('.notification-wrapper').fadeOut('slow', function () {
-				$(this).remove();
-			});
-		}, 5000);
-	}
-
-	var createNotification = function (message) {
-		var html = '';
-
-		html += '<div class="notification-wrapper">';
-		html += '	<div class="notification">';
-		html += '		<p class="notification__copy">' + message + '</p>';
-		html += '	</div>';
-		html += '</div>';
-
-		return html;
 	}
 
 	return {
