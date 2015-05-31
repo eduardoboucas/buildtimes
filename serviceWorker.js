1e505a15437e005ec0f603f79ee66f0e3fd60094
Eduardo Bouças <mail@eduardoboucas.com>
2015-05-31 11:57:11 +0100

    Service Worker (WIP)

--- serviceWorker.js
+++ serviceWorker.js
@@ -1,6 +1,9 @@
+---
+layout: null
+---
 importScripts('js/vendor/serviceworker-cache-polyfill.js');
 
-var CACHE_NAME = 'eduardoboucas.com-cache-v1';
+var CACHE_NAME = 'eduardoboucas.com-cache-v4';
 var urlsToCache = [
     // Stylesheets
     '/css/main.css',
@@ -11,37 +14,47 @@ var urlsToCache = [
     '/js/router.js',
     '/js/text.js',
 
-    // HTML pages
+    // Error page
+	'/404.html',
+    '/assets/images/glitch.png',
+
+    // Pages and essential assets
     '/index.html',
     '/home.html',
-    '/404.html',
     '/about.html',
+    '/assets/images/drawing.png',
     '/findme.html',
+
+    // Portfolio assets
     '/portfolio.html',
+    {% for project in site.portfolio %}'/assets/images/portfolio/thumbnails/{{ project.thumbnail }}',
+    '/assets/images/portfolio/screenshots/{{ project.screenshot }}',
+    '/portfolio/{{ project.handle }}.html',
+    {% endfor %}
 
     // Video
     '/js/VideoLooper.js',
     '/assets/videos/Part1.mp4',
     '/assets/videos/Part2.mp4',
-    '/assets/videos/Part3.mp4',
+    '/assets/videos/Part3.mp4'
 
     // Typography
-    'https://fonts.googleapis.com/css?family=Lato:400,700',
-    'https://fonts.googleapis.com/css?family=Economica:700'
+    //'https://fonts.googleapis.com/css?family=Lato:400,700',
+    //'https://fonts.googleapis.com/css?family=Economica:700'
 ];
 
 self.addEventListener('install', function(event) {
 	console.log('* Installing Service Worker...');
 
 	event.waitUntil(
-	caches.open(CACHE_NAME)
-	  .then(function(cache) {
-	    console.log('* Opened cache');
-	    return cache.addAll(urlsToCache).then(function () {
-	    	alert('Files have been cached!');
-	    });
-	  })
-	);
+		caches.open(CACHE_NAME)
+			.then(function(cache) {
+		    	console.log('* Opened cache');
+		    	return cache.addAll(urlsToCache).then(function () {
+		    		console.log('** [!] Files have been cached!');
+		    	});
+		  	})
+		);
 });
 
 self.addEventListener('fetch', function(event) {
@@ -50,11 +63,11 @@ self.addEventListener('fetch', function(event) {
 			.then(function(response) {
 				// Cache hit - return response
 				if (response) {
-					console.log('* Delivering cached response: ' + event.request.url);
+					console.log('** [CACHED]: ' + event.request.url);
 					return response;
 				}
 
-				console.log('* Fetching asset: ' + event.request.url);
+				console.log('** [Fetching]: ' + event.request.url);
 				return fetch(event.request);
 			}
 		)
