d9b0898e6ed7e6e4c396167c2ff0251e3a35f5fe
Eduardo Bouças <mail@eduardoboucas.com>
2017-02-25 11:24:14 +0000

    Add Video section

--- _src/main.js
+++ _src/main.js
@@ -199,9 +199,9 @@ var eb = (function ($) {
   function initSearch() {
     var resultTemplate = '';
 
-    resultTemplate += '<a href="{url}" class="search-result">';
-    resultTemplate += ' <p class="search-result__date">{date}</p>';
-    resultTemplate += ' <p class="search-result__title">{title}</p>';
+    resultTemplate += '<a href="{url}" class="search-result small-card">';
+    resultTemplate += ' <p class="small-card__pre">{date}</p>';
+    resultTemplate += ' <p class="small-card__title">{title}</p>';
     resultTemplate += '</a>';
 
     SimpleJekyllSearch({
---
+++ _tv/2016-08-10-staticman.md
@@ -0,0 +1,5 @@
+---
+title: Staticman
+post: 2016-08-10-staticman
+src: https://player.vimeo.com/video/178244050
+---
